7c9b102b8c8619414af463e0a010c02a226a010b
Béhr Levente <sidolevente@aol.com>
2024-10-15 14:11:42 +0200

    logika felépítése

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import osztalyok
+from osztalyok import *
 
-auto1 = osztalyok.Szemely("ABC123","Audi","A8",20000,200,5)
-auto2 = osztalyok.Teher("CBA456","Ford","Transit",15000,800,"B")
-print(auto1.tipus)
+# Kölcsönzők példányosítása
+Gyor = Kolcsonzo("Győr")
+Budapest = Kolcsonzo("Budapest")
+Debrecen = Kolcsonzo("Debrecen")
+
+# Autók példányosítása
+Audi_A8 = osztalyok.Szemely("ABC123","Audi","A8",20000,200,5)
+Ford_Transit = osztalyok.Teher("CBA456","Ford","Transit",15000,800,"B")
+
+# Autók regisztrálása az egyes kölcsönzőkbe
+Gyor.reg(Audi_A8)
+Gyor.reg(Ford_Transit)
+
+print("Üdvözöljük a kölcsönzőrendszerben\n\n")
+print("Írja be a kölcsönző kódját!\nTelephely      Kód\n\nGyőr           1\nBudapest       2\nDebrecen       3\n\n")
+kvalaszt=input("Telepkód: ")
+if kvalaszt == "1":
+    lista = Gyor.listazas()
+    print("A Győri kölcsönzőben jelenleg az alábbi autók elérhetőek:")
+    for kocsi in lista:
+        print(kocsi)
+if kvalaszt == "2":
+    print(f"A Budapesti kölcsönzőben jelenleg az alábbi autók elérhetőek: {Budapest.listazas()}")
+if kvalaszt == "3":
+    print(f"A Debreceni kölcsönzőben jelenleg az alábbi autók elérhetőek: {Debrecen.listazas()}")
+
+
+berlet = Berles(Audi_A8,"2024.10.15",2)
+#print (berlet)
+#print(f"Bérlés ára: {berles1.uj()} Forint")
--- osztalyok.py
+++ osztalyok.py
@@ -5,25 +5,38 @@ class Auto:
         self.tipus = tipus
         self.dij = dij
 
-
 class Szemely(Auto):
     def __init__(self,rendszam,marka,tipus,dij,csomagter,ajtok):
         super().__init__(rendszam,marka,tipus,dij)
         self.csomagter = csomagter
         self.ajtok = ajtok
-
+    def adatok(self):
+        return f"Személyautó - Rendszám: {self.rendszam}, Márka: {self.marka}, Típus: {self.tipus}, Napidíj: {self.dij}."
 
 class Teher(Auto):
     def __init__(self,rendszam,marka,tipus,dij,rakter,kat):
         super().__init__(rendszam,marka,tipus,dij)
         self.rakter = rakter
         self.kat = kat
+    def adatok(self):
+        return f"{self.marka} {self.tipus} teherautó - Rendszám: {self.rendszam}, Raktér: {self.rakter} m^2, Jog.kat.: {self.kat} Napidíj: {self.dij}."
 
 class Kolcsonzo:
-    def __init__(self,nev,):
+    def __init__(self,nev):
         self.nev = nev
         self.autok = []
-    def ujauto(self,auto):
+    def reg(self,auto):
         self.autok.append(auto)
     def listazas(self):
-        return [auto.adat() for auto in self.autok]
+        return [auto.adatok() for auto in self.autok]
+
+class Berles(Kolcsonzo):
+    def __init__(self,auto,datum,napok):
+        self.auto = auto
+        self.datum = datum
+        self.napok = napok
+        self.osszeg = self.auto.dij * self.napok
+    def __str__(self):
+        return f"Az aktuális bérlet teljes díja {self.osszeg} Forint"
+    def uj(self):
+        return self.auto.dij * self.napok
